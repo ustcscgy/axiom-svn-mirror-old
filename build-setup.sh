1fe63fd044d4479fa357933bbfd277d29d89d964
dos-reis <dos-reis@54bea96e-1511-0410-8851-aaeae44645fa>
2007-07-29 05:56:40 +0000

    * build-setup.sh: Conservatively re-build autogenerated build         system files.
    git-svn-id: https://axiom.svn.sourceforge.net/svnroot/axiom/branches/build-improvements@674 54bea96e-1511-0410-8851-aaeae44645fa

--- build-setup.sh
+++ build-setup.sh
@@ -7,14 +7,16 @@ error() {
 
 # set -x
 
-notangle ./configure.ac.pamphlet > ./configure.ac \
+notangle ./configure.ac.pamphlet > ./tmp-configure.ac \
    || error "could not extract configure.ac from pamphlet file"
+config/move-if-change tmp-configure.ac configure.ac
 
 autoheader || error "could not re-generate config/axiom-c-macros.h"
 autoconf || error "could not re-generate configure"
 
-## subdirectoris that contain Makefile pamphlets of interest
-SUBDIRS=". src                           \
+## subdirectories that contain Makefile pamphlets of interest
+SUBDIRS="     .   \
+                  src                    \
                   src/lib                \
 		  src/lisp               \
 		  src/boot               \
@@ -38,8 +40,9 @@ SUBDIRS=". src                           \
 
 
 for d in $SUBDIRS; do
-    notangle -t8 $d/Makefile.pamphlet > $d/Makefile.in \
+    notangle -t8 $d/Makefile.pamphlet > $d/tmp-Makefile.in \
        || error "could not extract $d/Makefile.in from pamphlet file"
+    config/move-if-change $d/tmp-Makefile.in $d/Makefile.in
 done
 
 # set +x
